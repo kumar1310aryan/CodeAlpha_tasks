1b5d1c20c2c155568a9591f304f4d9c1440607d1
kumar1310aryan <kumar1310aryan@gmail.com>
2024-06-16 17:55:33 +0530

    Book Library

--- Book_Library/script.js
+++ Book_Library/script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener("DOMContentLoaded", () => {
   const bookList = document.getElementById("book-list");
   const categoryList = document.getElementById("category-list");
